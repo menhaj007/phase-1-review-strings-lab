6d52aa7493650789e311a024598dd953cb556414
menhaj <menhaj007@yahoo.com>
2021-04-05 14:47:18 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "James";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+// let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// let shortGreeting = "Welcome, " + currentUser + "!";
+let shortGreeting = welcomeMessage.slice(0,7) +", " + currentUser[0] + "!";
+console.log(shortGreeting);
